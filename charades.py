02577ec57c1d7c3c4ee5a723aa2fae1799cb95c3
Nick <nwrabon@gmail.com>
2021-11-30 19:25:23 -0500

    added dictionary API support

--- charades.py
+++ charades.py
@@ -1,10 +1,17 @@
 import json
-
+from PyDictionary import PyDictionary
 import requests
 
 
 def generate_random_word():
     return str((requests.get('https://random-word-api.herokuapp.com/word?number=1&swear=0').json())).replace('\'', '').replace('[','').replace(']','')
 
+def print_word_info(word):
+    dictionary = PyDictionary()
+    print("Word Info:")
+    print(dictionary.meaning(word))
+    
 
-print(generate_random_word())
+word = generate_random_word()
+print(word)
+print_word_info(word)
